db656ab7bb5cee7fa84f033cf0b19afec65117e6
“Ali <tadayoni.ali.enayat@gmail.com>
2024-11-20 12:24:26 +0330

    Implemented Routes With Views

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import CartView from "@/views/CartView.vue";
+import CheckoutView from "@/views/CheckoutView.vue";
+import NotFoundView from "@/views/NotFoundView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +11,21 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+    },
+    {
+      path: "/cart",
+      name: "cart",
+      component: CartView,
+    },
+    {
+      path: "/checkout",
+      name: "checkout",
+      component: CheckoutView,
+    },
+    {
+      path: "/:catchAll(.*)",
+      name: "not-found",
+      component: NotFoundView,
     },
   ],
 });
